50dce2e15a531ecff7a490ff6b63f2f111166c9d
hzokbe <hzokbe@gmail.com>
2025-09-15 19:04:29 -0300

    feat(stack): add function to get new stack

--- stack/stack.c
+++ stack/stack.c
@@ -2,7 +2,24 @@
 
 #include "../node/node.h"
 
+#include <stdlib.h>
+
 typedef struct Stack
 {
   Node* top;
 } Stack;
+
+Stack* new_stack(void)
+{
+  Stack* s = malloc(sizeof(Stack));
+
+  if (!s)
+  {
+    return NULL;
+  }
+
+  s->top = NULL;
+
+  return s;
+}
+
