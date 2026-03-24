af7768db8ca3f10044c5de71f9b94a615b7718fc
hzokbe <hzokbe@gmail.com>
2026-03-23 21:39:20 -0300

    feat(auth-service/service): add user service

---
+++ auth-service/src/main/java/com/hzokbe/hayai/auth/service/user/UserService.java
@@ -0,0 +1,11 @@
+package com.hzokbe.hayai.auth.service.user;
+
+import com.hzokbe.hayai.auth.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository repository;
+}
